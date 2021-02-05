ceed09144d98388007c96a523d0c406411031f7c
Mario Salgado <mapsalgado92@gmail.com>
2021-02-05 09:53:17 +0000

    Secrets hiden

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDmZSo7o8WB-bsge5T_Rd9VJdODhA-wD2Y",
-    authDomain: "my-firebase-1f789.firebaseapp.com",
-    projectId: "my-firebase-1f789",
-    messagingSenderId: "917517905132",
-    appId: "1:917517905132:web:2cc22f398c1a2511a209c9",
-    measurementId: "G-7N4N4SG1FZ"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
+  }
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
